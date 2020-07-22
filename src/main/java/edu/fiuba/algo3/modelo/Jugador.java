1ca775899eaedf6cc9a3892797ff9a25eb02d583
Zaphyk <zaphyk@live.com>
2020-07-21 22:38:17 -0300

    add jugador code

--- src/main/java/edu/fiuba/algo3/modelo/Jugador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Jugador.java
@@ -1,4 +1,35 @@
 package edu.fiuba.algo3.modelo;
 
+import java.util.List;
+
 public class Jugador {
+
+    private List<String> respuestas;
+    private int puntaje;
+    private String nombre;
+
+    public Jugador(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public void agregarRespuestas(List<String> respuestas)
+    {
+        this.respuestas = respuestas;
+    }
+
+    public int obtenerPuntaje()
+    {
+        return this.puntaje;
+    }
+
+    public List<String> getRespuestas()
+    {
+        return this.respuestas;
+    }
+
+    public void agregarPuntaje(int puntaje)
+    {
+        this.puntaje = puntaje;
+    }
 }
