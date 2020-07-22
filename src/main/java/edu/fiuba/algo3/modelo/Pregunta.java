e760f158eb5b5f93e98e4c83089aef42a80ba61d
Mathias <welzmathias@gmail.com>
2020-07-21 22:48:23 -0300

    Agrego clase Pregunta

--- src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
@@ -1,4 +1,29 @@
 package edu.fiuba.algo3.modelo;
 
+import java.util.List;
+
 public class Pregunta {
+    String pregunta;
+    List<String> opciones;
+    List<String> respuestas;
+    Tipo tipo;
+    Modo modo;
+
+    public Pregunta(String pregunta, List<String> opciones, List<String> respuestas,Tipo tipo,Modo modo) {
+        this.pregunta = pregunta;
+        this.opciones = opciones;
+        this.respuestas = respuestas;
+        this.tipo = tipo;
+        this.modo = modo;
+    }
+    public List<String> getRespuesta() {
+        return respuestas;
+    }
+
+    public double calcularPuntaje(List<String> respuestasJugador){
+        if(respuestas.equals(respuestasJugador)){
+            return 1;
+        }
+        return 0;
+    }
 }
