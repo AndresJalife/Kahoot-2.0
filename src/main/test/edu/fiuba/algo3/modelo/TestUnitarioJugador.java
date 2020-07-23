a5614349f3c4a38b56d093f78599a23daabf6068
AgustinBrasburg <agus_yo_soy@hotmail.com>
2020-07-23 18:48:33 -0300

    Test Unitarios de Jugador y Preguntas vacios

--- src/main/test/edu/fiuba/algo3/modelo/TestUnitarioJugador.java
+++ src/main/test/edu/fiuba/algo3/modelo/TestUnitarioJugador.java
@@ -2,9 +2,6 @@ package edu.fiuba.algo3.modelo;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestUnitarioJugador {
