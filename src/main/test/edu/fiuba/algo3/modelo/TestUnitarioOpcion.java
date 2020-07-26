ede34e41a62ecc6543405123df422afa26fa0246
AgustinBrasburg <agus_yo_soy@hotmail.com>
2020-07-26 18:16:10 -0300

    TestUnitarioOpcion CrearOpcionyAlDecirleQueEsLaRespuestaDaTrue

--- src/main/test/edu/fiuba/algo3/modelo/TestUnitarioOpcion.java
+++ src/main/test/edu/fiuba/algo3/modelo/TestUnitarioOpcion.java
@@ -3,6 +3,7 @@ package edu.fiuba.algo3.modelo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestUnitarioOpcion {
 
@@ -10,5 +11,12 @@ public class TestUnitarioOpcion {
     public void CrearOpcionyEsFalso(){
         Opcion opcion = new Opcion("");
         assertFalse(opcion.Respuesta());
+    }
+
+    @Test
+    public void CrearOpcionyAlDecirleQueEsLaRespuestaDaTrue(){
+        Opcion opcion = new Opcion("");
+        opcion.Opcion_Correcta();
+        assertTrue(opcion.Respuesta());
     }
 }
