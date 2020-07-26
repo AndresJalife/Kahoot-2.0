c1c7d94e1596ea84947638c814103bf770decd9a
AgustinBrasburg <agus_yo_soy@hotmail.com>
2020-07-26 16:49:59 -0300

    Modificaciones en Pregunta para que acepte Opcion

--- src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
@@ -1,28 +1,33 @@
 package edu.fiuba.algo3.modelo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pregunta {
     String pregunta;
     List<Opcion> opciones;
-    List<Opcion> respuestas;
     ITipoDePregunta tipo;
     IModoDePregunta modo;
 
-    public Pregunta(String pregunta, List<Opcion> opciones, List<Opcion> respuestas,ITipoDePregunta tipo,IModoDePregunta modo) {
+    public Pregunta(String pregunta, List<Opcion> opciones,ITipoDePregunta tipo,IModoDePregunta modo) {
         this.pregunta = pregunta;
         this.opciones = opciones;
-        this.respuestas = respuestas;
         this.tipo = tipo;
         this.modo = modo;
     }
     public List<Opcion> getRespuesta() {
+        List<Opcion> respuestas = new ArrayList<>();
+        for(Opcion opcion:opciones){
+            if(opcion.Respuesta())
+                respuestas.add(opcion);
+        }
         return respuestas;
     }
 
     public int calcularPuntaje(List<Opcion> respuestasJugador){
-        if(respuestas.equals(respuestasJugador)){
-            return 1;
+        for(Opcion opcion:opciones){
+            if(opcion.Respuesta())
+                return 1;
         }
         return 0;
     }
