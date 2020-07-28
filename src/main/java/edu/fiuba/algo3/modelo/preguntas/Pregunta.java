d3517a4bd648c7f00e759df96d4f8abc1d63cddd
Zaphyk <zaphyk@live.com>
2020-07-27 22:06:52 -0300

    refactor clase pregunta

--- src/main/java/edu/fiuba/algo3/modelo/preguntas/Pregunta.java
+++ src/main/java/edu/fiuba/algo3/modelo/preguntas/Pregunta.java
@@ -1,15 +1,11 @@
 package edu.fiuba.algo3.modelo.preguntas;
 
-import edu.fiuba.algo3.modelo.IModoDePregunta;
-import edu.fiuba.algo3.modelo.ITipoDePregunta;
-import edu.fiuba.algo3.modelo.Opcion;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Pregunta {
     private String pregunta;
-    private Opcion[] opcionesPosibles;
+    private Opcion[] respuestasPosibles;
     private ITipoDePregunta tipo;
     private IModoDePregunta modo;
     private int tiempo;
@@ -18,21 +14,25 @@ public class Pregunta {
 
     public Pregunta(String pregunta, Opcion[] opcionesPosibles, int tiempo, int puntaje, ITipoDePregunta tipo, IModoDePregunta modo) {
         this.pregunta = pregunta;
-        this.opcionesPosibles = opcionesPosibles;
+        this.respuestasPosibles = opcionesPosibles;
         this.puntaje = puntaje;
         this.tiempo = tiempo;
         this.tipo = tipo;
         this.modo = modo;
     }
 
-    public Opcion[] obtenerOpciones() {
-        return opcionesPosibles;
+    public int obtenerTiempo() {
+        return tiempo;
     }
 
-    public Opcion[] obtenerRespuestasCorrecta() {
+    public Opcion[] obtenerOpciones() {
+        return respuestasPosibles;
+    }
+
+    public Opcion[] obtenerRespuestasCorrectas() {
         List<Opcion> respuestas = new ArrayList<>();
-        for(var opcion : opcionesPosibles) {
-            if(opcion.Respuesta())
+        for(var opcion : respuestasPosibles) {
+            if(opcion.esCorrecta())
                 respuestas.add(opcion);
         }
         return respuestas.toArray(Opcion[]::new);
