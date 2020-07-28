9162f686dcaf61d4ba6aec3ff87d747060794ea1
Zaphyk <zaphyk@live.com>
2020-07-27 22:06:06 -0300

    añadi con penalidad

--- src/main/java/edu/fiuba/algo3/modelo/preguntas/ModoConPenalidad.java
+++ src/main/java/edu/fiuba/algo3/modelo/preguntas/ModoConPenalidad.java
@@ -1,4 +1,10 @@
 package edu.fiuba.algo3.modelo.preguntas;
 
-public class ModoConPenalidad {
+public class ModoConPenalidad implements IModoDePregunta {
+
+    @Override
+    public int calcularPuntaje(int respuestasCorrectasTotales, int respuestasIncorrectasJugador, int respuestasCorrectasJugador) {
+        /* Suma puntos por correctas y resta por incorrectas */
+        return respuestasCorrectasJugador - respuestasIncorrectasJugador;
+    }
 }
