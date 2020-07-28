15249585c1ccb6df41c36158bd5a27215145d783
AgustinBrasburg <agus_yo_soy@hotmail.com>
2020-07-28 15:34:08 -0300

    Prueba MultipleChoiceClasicoTest contesta mal

--- src/main/test/edu/fiuba/algo3/modelo/MultipleChoiceClasicoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/MultipleChoiceClasicoTest.java
@@ -52,4 +52,26 @@ public class MultipleChoiceClasicoTest {
 
         assertEquals(0, jugador.obtenerPuntaje());
     }
+
+    @Test
+    public void ingresarRespuestaVerdaderoFalsoClasicoYContestaMal() {
+        List<Opcion> opciones = new ArrayList<>();
+        List<Opcion> respuestas = new ArrayList<>();
+        Jugador jugador = new Jugador("Jorge");
+        Opcion Opcion1 = new Opcion("Si", true);
+        Opcion Opcion2 = new Opcion("No");
+        Opcion Opcion3 = new Opcion("Si, es correcta", true);
+
+        opciones.add(Opcion1);
+        opciones.add(Opcion2);
+        opciones.add(Opcion3);
+
+        var pregunta = new Pregunta("Francia es un pais", opciones, 1, 1, new TipoMultipleChoice(), new ModoClasico());
+        /* Selecciona la primer respuesta */
+        respuestas.add(Opcion2);
+
+        jugador.agregarPuntaje(pregunta.calcularPuntaje(respuestas));
+
+        assertEquals(0, jugador.obtenerPuntaje());
+    }
 }
