8eec7e1ae4d99b8c90641120743415e7431b7e9e
Zaphyk <zaphyk@live.com>
2020-07-27 22:25:56 -0300

    unit tests clasicos

--- src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestUnitarioModo.java
+++ src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestUnitarioModo.java
@@ -1,4 +1,16 @@
 package edu.fiuba.algo3.modelo.TestUnitarios;
 
 public class TestUnitarioModo {
+    @Test
+    public void TestModoClasicoNoDaPuntajeSiNoTieneTodas() {
+        var modo = new ModoClasico();
+        assertEquals(0, modo.calcularPuntaje(5, 0, 4));
+    }
+
+    @Test
+    public void TestModoClasicoDaPuntaConTodas() {
+        var modo = new ModoClasico();
+        assertEquals(5, modo.calcularPuntaje(5, 0, 5));
+    }
+
 }
