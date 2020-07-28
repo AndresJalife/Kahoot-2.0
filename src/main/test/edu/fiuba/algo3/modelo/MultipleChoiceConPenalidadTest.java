ea9d775869123a410db3f57f27e07308c1b0c465
DamianGanopolsky <damian98dgg@gmail.com>
2020-07-28 19:46:38 -0300

    Update tests

--- src/main/test/edu/fiuba/algo3/modelo/MultipleChoiceConPenalidadTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/MultipleChoiceConPenalidadTest.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.modelo.general.Jugador;
 import edu.fiuba.algo3.modelo.preguntas.*;
 import org.junit.jupiter.api.Test;
 
@@ -8,10 +9,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MultipleChoiceConParcialConPenalidadTest {
+public class MultipleChoiceConPenalidadTest {
 
     @Test
-    public void ingresarRespuestaMultipleChoiceConParcialConPenalidad() {
+    public void ingresarRespuestaMultipleChoiceConPenalidad() {
         List<Opcion> opciones = new ArrayList<>();
         List<Opcion> respuestas = new ArrayList<>();
         var Opcion1 = new Opcion("Si", true);
@@ -28,4 +29,94 @@ public class MultipleChoiceConParcialConPenalidadTest {
 
         assertEquals(respuestas, pregunta.obtenerRespuestasCorrectas());
     }
+
+    @Test
+    public void ingresarRespuestaMultipleChoiceConPenalidadYContestaUnaBien() {
+        List<Opcion> opciones = new ArrayList<>();
+        List<Opcion> respuestas = new ArrayList<>();
+        Jugador jugador = new Jugador("Sebastian");
+        Opcion Opcion1 = new Opcion("Si", true);
+        Opcion Opcion2 = new Opcion("No");
+        Opcion Opcion3 = new Opcion("Si, es correcta", true);
+
+        opciones.add(Opcion1);
+        opciones.add(Opcion2);
+        opciones.add(Opcion3);
+
+        var pregunta = new Pregunta("Francia es un pais", opciones, 1, 1, new TipoMultipleChoice(), new ModoConPenalidad());
+        respuestas.add(Opcion1);
+
+        jugador.agregarPuntaje(pregunta.calcularPuntaje(respuestas));
+
+        assertEquals(1, jugador.obtenerPuntaje());
+    }
+
+
+    @Test
+    public void ingresarRespuestaMultipleChoiceConPenalidadYContestaDosBien() {
+        List<Opcion> opciones = new ArrayList<>();
+        List<Opcion> respuestas = new ArrayList<>();
+        Jugador jugador = new Jugador("Sebastian");
+        Opcion Opcion1 = new Opcion("Si", true);
+        Opcion Opcion2 = new Opcion("No");
+        Opcion Opcion3 = new Opcion("Si, es correcta", true);
+
+        opciones.add(Opcion1);
+        opciones.add(Opcion2);
+        opciones.add(Opcion3);
+
+        var pregunta = new Pregunta("Francia es un pais", opciones, 1, 1, new TipoMultipleChoice(), new ModoConPenalidad());
+        respuestas.add(Opcion1);
+        respuestas.add(Opcion3);
+
+        jugador.agregarPuntaje(pregunta.calcularPuntaje(respuestas));
+
+        assertEquals(2, jugador.obtenerPuntaje());
+    }
+
+    @Test
+    public void ingresarRespuestaMultipleChoiceYContestaUnaMal() {
+        List<Opcion> opciones = new ArrayList<>();
+        List<Opcion> respuestas = new ArrayList<>();
+        Jugador jugador = new Jugador("Edgardo");
+        Opcion Opcion1 = new Opcion("Si", true);
+        Opcion Opcion2 = new Opcion("No");
+        Opcion Opcion3 = new Opcion("Si, es correcta", true);
+
+        opciones.add(Opcion1);
+        opciones.add(Opcion2);
+        opciones.add(Opcion3);
+
+        var pregunta = new Pregunta("Francia es un pais", opciones, 1, 1, new TipoMultipleChoice(), new ModoConPenalidad());
+
+        respuestas.add(Opcion2);
+
+        jugador.agregarPuntaje(pregunta.calcularPuntaje(respuestas));
+
+        assertEquals(-1, jugador.obtenerPuntaje());
+    }
+
+    @Test
+    public void ingresarRespuestaMultipleChoiceYContestaUnaMalYUnaBien() {
+        List<Opcion> opciones = new ArrayList<>();
+        List<Opcion> respuestas = new ArrayList<>();
+        Jugador jugador = new Jugador("Edgardo");
+        Opcion Opcion1 = new Opcion("Si", true);
+        Opcion Opcion2 = new Opcion("No");
+        Opcion Opcion3 = new Opcion("Si, es correcta", true);
+
+        opciones.add(Opcion1);
+        opciones.add(Opcion2);
+        opciones.add(Opcion3);
+
+        var pregunta = new Pregunta("Francia es un pais", opciones, 1, 1, new TipoMultipleChoice(), new ModoClasico());
+        respuestas.add(Opcion1);
+        respuestas.add(Opcion2);
+
+        jugador.agregarPuntaje(pregunta.calcularPuntaje(respuestas));
+
+        assertEquals(0, jugador.obtenerPuntaje());
+    }
+
+
 }
