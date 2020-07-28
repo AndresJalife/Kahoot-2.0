bbefd2b6a61cb6f508634241a34c4c1b88beb2e0
Zaphyk <zaphyk@live.com>
2020-07-27 22:08:01 -0300

    arreglar comment

--- src/main/java/edu/fiuba/algo3/modelo/preguntas/ModoPuntajeParcial.java
+++ src/main/java/edu/fiuba/algo3/modelo/preguntas/ModoPuntajeParcial.java
@@ -3,7 +3,7 @@ package edu.fiuba.algo3.modelo.preguntas;
 public class ModoPuntajeParcial implements IModoDePregunta {
     @Override
     public int calcularPuntaje(int respuestasCorrectasTotales, int respuestasIncorrectasJugador, int respuestasCorrectasJugador) {
-        /* Suma parcialmente *(
+        /* Suma parcialmente */
         return respuestasCorrectasJugador * (1 - Math.min(respuestasIncorrectasJugador, 1));
     }
 }
