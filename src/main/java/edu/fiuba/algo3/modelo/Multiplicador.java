7e60a5e24c645348ca35e05873bbaa527b54128a
DamianGanopolsky <damian98dgg@gmail.com>
2020-07-28 00:02:24 -0300

    Refactor

--- src/main/java/edu/fiuba/algo3/modelo/Multiplicador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Multiplicador.java
@@ -1,4 +1,11 @@
 package edu.fiuba.algo3.modelo;
 
 public class Multiplicador implements IModificador {
+
+    private int cantUsos=1;
+
+    public Boolean quedanUsos(){
+        return (cantUsos!=0);
+    }
+
 }
--- src/main/java/edu/fiuba/algo3/modelo/Multiplicadorx3.java
+++ src/main/java/edu/fiuba/algo3/modelo/Multiplicadorx3.java
@@ -2,9 +2,5 @@ package edu.fiuba.algo3.modelo;
 
 public class Multiplicadorx3 extends Multiplicador {
 
-    private int cantUsos=1;
 
-    public Boolean quedanUsos(){
-        return (cantUsos!=0);
-    }
 }
