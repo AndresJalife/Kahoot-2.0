53b4656fa213d23409af4fc493b6c5cb9a2da3ba
AgustinBrasburg <agus_yo_soy@hotmail.com>
2020-07-28 00:05:10 -0300

    Pruebas del Nombre del Jugador

--- src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestUnitarioJugador.java
+++ src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestUnitarioJugador.java
@@ -1,8 +1,22 @@
 package edu.fiuba.algo3.modelo.TestUnitarios;
 
+import edu.fiuba.algo3.modelo.Jugador;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestUnitarioJugador {
+
+    @Test
+    public void CrearJugadorConNombre(){
+        Jugador jugador = new Jugador("Juan Perez");
+        assertTrue(true);
+        // Prueba que deberia dar siempre positivo para ver confirmar si lo que falla son las pruebas o otra cosa
+    }
+
+    @Test
+    public void CrearJugadorConNombreVacio(){
+        Jugador jugador = new Jugador("");
+        assertTrue(true);
+    }
 }
