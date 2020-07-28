9af17a6ccfdb5362a1aa1a74d600ab4b3d6dfc02
Zaphyk <zaphyk@live.com>
2020-07-27 22:07:47 -0300

    imports

--- src/main/test/edu/fiuba/algo3/modelo/VerdaderoFalsoPenalidadTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/VerdaderoFalsoPenalidadTest.java
@@ -1,5 +1,8 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.modelo.preguntas.IModoDePregunta;
+import edu.fiuba.algo3.modelo.preguntas.ITipoDePregunta;
+import edu.fiuba.algo3.modelo.preguntas.Opcion;
 import edu.fiuba.algo3.modelo.preguntas.Pregunta;
 import org.junit.jupiter.api.Test;
 
