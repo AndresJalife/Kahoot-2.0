cf4496a8a4365cf764bd9a74b9a573d9fc285d7b
Andres Jalife <ajalife@fi.uba.ar>
2020-07-29 19:09:52 -0300

    hay que borrar esta clase

--- src/main/java/edu/fiuba/algo3/modelo/general/IRonda.java
+++ src/main/java/edu/fiuba/algo3/modelo/general/IRonda.java
@@ -1,10 +1,11 @@
 package edu.fiuba.algo3.modelo.general;
 
 public interface IRonda {
-    void comenzar();
-    void mostrarPregunta();
-    void pedirRespuesta(Jugador jugador);
-    void mostrarRespuestaCorrecta();
-    void mostrarPosiblesRespuestas();
-    void mostrarModificadores(Jugador jugador);
+//    void comenzar();
+//    void mostrarPregunta();
+//    void pedirRespuesta(Jugador jugador);
+//    void mostrarRespuestaCorrecta();
+//    void mostrarPosiblesRespuestas();
+//    void mostrarModificadores(Jugador jugador);
+//    HAY QUE BORRAR ESTA CLASE SI QUEDA RondaBase
 }
