259506c8106c885f395184cfe05128d97b4d064a
Mathias <welzmathias@gmail.com>
2020-07-30 15:17:23 -0300

    muevo FabricaDePreguntas a paquete preguntas

--- src/main/java/edu/fiuba/algo3/modelo/preguntas/FabricaDePreguntas.java
+++ src/main/java/edu/fiuba/algo3/modelo/preguntas/FabricaDePreguntas.java
@@ -1,4 +1,4 @@
-package edu.fiuba.algo3.modelo;
+package edu.fiuba.algo3.modelo.preguntas;
 
 import edu.fiuba.algo3.modelo.preguntas.*;
 
@@ -6,18 +6,25 @@ import java.util.List;
 
 public class FabricaDePreguntas {
     public Pregunta crearPregunta(String pregunta, List<Opcion> opciones,int tiempo,int puntajePorRespuesta, String tipoPregunta, String modoPregunta) {
+
         if (tipoPregunta.equalsIgnoreCase("VerdaderoFalso")) {
+
             if (modoPregunta.equalsIgnoreCase("Clasico")) {
                 return new Pregunta(pregunta, opciones, tiempo,puntajePorRespuesta,new TipoVerdaderoYFalso(), new ModoClasico());
-            } else if (modoPregunta.equalsIgnoreCase("ConPenalidad")) {
+            }
+            else if (modoPregunta.equalsIgnoreCase("ConPenalidad")) {
                 return new Pregunta(pregunta, opciones, tiempo,puntajePorRespuesta, new TipoVerdaderoYFalso(), new ModoConPenalidad());
             }
-        }else if(tipoPregunta.equalsIgnoreCase("MultipleChoice")){
+        }
+        else if(tipoPregunta.equalsIgnoreCase("MultipleChoice")){
+
             if (modoPregunta.equalsIgnoreCase("Clasico")) {
                 return new Pregunta(pregunta, opciones, tiempo,puntajePorRespuesta,new TipoMultipleChoice(), new ModoClasico());
-            } else if (modoPregunta.equalsIgnoreCase("ConPenalidad")) {
+            }
+            else if (modoPregunta.equalsIgnoreCase("ConPenalidad")) {
                 return new Pregunta(pregunta, opciones, tiempo,puntajePorRespuesta, new TipoMultipleChoice(), new ModoConPenalidad());
-            }else if (modoPregunta.equalsIgnoreCase("ConPuntajeParcial")) {
+            }
+            else if (modoPregunta.equalsIgnoreCase("ConPuntajeParcial")) {
                 return new Pregunta(pregunta, opciones, tiempo, puntajePorRespuesta, new TipoMultipleChoice(), new ModoPuntajeParcial());
             }
         }
