52396687a7ef0eb67ba7910d1699523561bd5a21
Mathias <welzmathias@gmail.com>
2020-07-29 21:33:07 -0300

    agrego FabricaDePreguntas y Sus test unitarios

--- src/main/java/edu/fiuba/algo3/modelo/FabricaDePreguntas.java
+++ src/main/java/edu/fiuba/algo3/modelo/FabricaDePreguntas.java
@@ -1,14 +1,24 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.modelo.preguntas.*;
+
 import java.util.List;
 
 public class FabricaDePreguntas {
-    public Pregunta obtenerPregunta(String pregunta, List<Opcion> opciones, String tipoPregunta, String modoPregunta) {
+    public Pregunta crearPregunta(String pregunta, List<Opcion> opciones,int tiempo,int puntajePorRespuesta, String tipoPregunta, String modoPregunta) {
         if (tipoPregunta.equalsIgnoreCase("VerdaderoFalso")) {
             if (modoPregunta.equalsIgnoreCase("Clasico")) {
-                return new Pregunta(pregunta, opciones, new VerdaderoFalso(), new Clasico());
+                return new Pregunta(pregunta, opciones, tiempo,puntajePorRespuesta,new TipoVerdaderoYFalso(), new ModoClasico());
             } else if (modoPregunta.equalsIgnoreCase("ConPenalidad")) {
-                return new Pregunta(pregunta, opciones, new VerdaderoFalso(), new ConPenalidad());
+                return new Pregunta(pregunta, opciones, tiempo,puntajePorRespuesta, new TipoVerdaderoYFalso(), new ModoConPenalidad());
+            }
+        }else if(tipoPregunta.equalsIgnoreCase("MultipleChoice")){
+            if (modoPregunta.equalsIgnoreCase("Clasico")) {
+                return new Pregunta(pregunta, opciones, tiempo,puntajePorRespuesta,new TipoMultipleChoice(), new ModoClasico());
+            } else if (modoPregunta.equalsIgnoreCase("ConPenalidad")) {
+                return new Pregunta(pregunta, opciones, tiempo,puntajePorRespuesta, new TipoMultipleChoice(), new ModoConPenalidad());
+            }else if (modoPregunta.equalsIgnoreCase("ConPuntajeParcial")) {
+                return new Pregunta(pregunta, opciones, tiempo, puntajePorRespuesta, new TipoMultipleChoice(), new ModoPuntajeParcial());
             }
         }
         return null;
---
+++ src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestUnitarioFabricaPreguntas.java
@@ -0,0 +1,80 @@
+package edu.fiuba.algo3.modelo.TestUnitarios;
+
+import edu.fiuba.algo3.modelo.FabricaDePreguntas;
+import edu.fiuba.algo3.modelo.preguntas.Opcion;
+import edu.fiuba.algo3.modelo.preguntas.Pregunta;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestUnitarioFabricaPreguntas {
+
+    FabricaDePreguntas fabrica = new FabricaDePreguntas();
+    String textoPregunta = "PREGUNTA";
+    Opcion opcion1 = new Opcion("opcion1", true);
+    Opcion opcion2 = new Opcion("opcion2");
+    Opcion opcion3 = new Opcion("opcion3");
+    List<Opcion> opciones = new ArrayList<>();
+    int tiempo = 1;
+    int puntajePorRespuestas = 1;
+
+    @Test
+    public void crearPreguntaVerdaderoFalsoClasicoConExito(){
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        Pregunta pregunta = fabrica.crearPregunta(textoPregunta,opciones,tiempo,puntajePorRespuestas,"VerdaderoFalso","Clasico");
+
+        assertNotNull(pregunta);
+    }
+
+    @Test
+    public void crearPreguntaVerdaderoFalsoConPenalidadConExito(){
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        Pregunta pregunta = fabrica.crearPregunta(textoPregunta,opciones,tiempo,puntajePorRespuestas,"VerdaderoFalso","conPenalidad");
+
+        assertNotNull(pregunta);
+    }
+
+    @Test
+    public void crearPreguntaMultipleChoiceClasicoConExito(){
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        Pregunta pregunta = fabrica.crearPregunta(textoPregunta,opciones,tiempo,puntajePorRespuestas,"MultipleChoice","clasico");
+
+        assertNotNull(pregunta);
+    }
+
+    @Test
+    public void crearPreguntaMultipleChoiceConPenalidadConExito(){
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        Pregunta pregunta = fabrica.crearPregunta(textoPregunta,opciones,tiempo,puntajePorRespuestas,"MultipleChoice","conPenalidad");
+
+        assertNotNull(pregunta);
+    }
+
+
+
+    @Test
+    public void crearPreguntaConNombreDelTipoIncorrectoDevuelveNull(){
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        Pregunta pregunta = fabrica.crearPregunta(textoPregunta,opciones,tiempo,puntajePorRespuestas,"verdadero y falso","conPenalidad");
+
+        assertNull(pregunta);
+    }
+
+    @Test
+    public void crearPreguntaConNombreDelModoIncorrectoDevuelveNull(){
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        Pregunta pregunta = fabrica.crearPregunta(textoPregunta,opciones,tiempo,puntajePorRespuestas,"VerdaderoFalso","Penalidad");
+
+        assertNull(pregunta);
+    }
+}
