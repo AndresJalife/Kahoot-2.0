954dd68e6165958cd962d1894085d759582bd649
DamianGanopolsky <damian98dgg@gmail.com>
2020-07-31 15:57:13 -0300

    Agrego clase Multip y metodos

--- src/main/java/edu/fiuba/algo3/modelo/general/Multiplicador.java
+++ src/main/java/edu/fiuba/algo3/modelo/general/Multiplicador.java
@@ -1,14 +1,18 @@
 package edu.fiuba.algo3.modelo.general;
 
-public abstract class Multiplicador implements IModificador {
+public class Multiplicador implements IModificador {
 
     private int usosDisponibles = 1;
+    private int factor;
 
     public Boolean quedanUsos(){
         return (usosDisponibles !=0);
     }
 
-    @Override
+    public Multiplicador(int factor) {
+        this.factor = factor;
+    }
+
     public void utilizar(){
         usosDisponibles -= 1;
     }
