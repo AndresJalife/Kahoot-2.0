99074d0b5c49152cd34e41cc03e1a3889a14e4c9
Andres Jalife <ajalife@fi.uba.ar>
2020-08-02 15:37:29 -0300

    actualizo test unitario en base a excepcion

--- src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestUnitarioMultiplicador.java
+++ src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestUnitarioMultiplicador.java
@@ -1,6 +1,7 @@
 package edu.fiuba.algo3.modelo.TestUnitarios;
 
 import edu.fiuba.algo3.modelo.general.Multiplicador;
+import edu.fiuba.algo3.modelo.general.NoQuedanUsosExcepcion;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,28 +21,28 @@ public class TestUnitarioMultiplicador {
     }
 
     @Test
-    public void CrearMultiplicadorx2YVerificarQueModificaCorrectamenteUnPuntaje(){
+    public void CrearMultiplicadorx2YVerificarQueModificaCorrectamenteUnPuntaje() throws NoQuedanUsosExcepcion {
         Multiplicador x2= new Multiplicador(2);
         int puntaje = 3;
         assertEquals(6, x2.modificarPuntaje(puntaje));
     }
 
     @Test
-    public void CrearMultiplicadorx3YVerificarQueModificaCorrectamenteUnPuntajePositivo(){
+    public void CrearMultiplicadorx3YVerificarQueModificaCorrectamenteUnPuntajePositivo() throws NoQuedanUsosExcepcion {
         Multiplicador x3= new Multiplicador(3);
         int puntaje = 3;
         assertEquals(9, x3.modificarPuntaje(puntaje));
     }
 
     @Test
-    public void CrearMultiplicadorx2YVerificarQueModificaCorrectamenteUnPuntajeNegativo(){
+    public void CrearMultiplicadorx2YVerificarQueModificaCorrectamenteUnPuntajeNegativo() throws NoQuedanUsosExcepcion {
         Multiplicador x2= new Multiplicador(2);
         int puntaje = -4;
         assertEquals(-8, x2.modificarPuntaje(puntaje));
     }
 
     @Test
-    public void UtilizoElMultiplicadorYNoLeQuedanUsos(){
+    public void UtilizoElMultiplicadorYNoLeQuedanUsos() throws NoQuedanUsosExcepcion {
         Multiplicador x2= new Multiplicador(2);
         x2.utilizar();
         assertFalse(x2.quedanUsos());
