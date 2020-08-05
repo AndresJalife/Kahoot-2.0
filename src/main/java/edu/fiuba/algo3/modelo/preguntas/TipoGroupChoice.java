1a29a750c58e6253201746fbe4e5add1fe037c4f
DamianGanopolsky <damian98dgg@gmail.com>
2020-08-04 23:54:54 -0300

    Agrego comportamiento a la clase GC

--- src/main/java/edu/fiuba/algo3/modelo/preguntas/TipoGroupChoice.java
+++ src/main/java/edu/fiuba/algo3/modelo/preguntas/TipoGroupChoice.java
@@ -1,4 +1,21 @@
 package edu.fiuba.algo3.modelo.preguntas;
 
-public class TipoGroupChoice {
+import java.util.List;
+
+public class TipoGroupChoice implements ITipoDePregunta{
+
+    @Override
+    public boolean opcionesValidas(List<Opcion> opciones) {
+
+        int contadorRespuestasCorrectas = 0;
+        if(opciones.size() >= 2 && opciones.size()<=5){
+            for(Opcion opcion: opciones){
+                if(opcion.esCorrecta()){
+                    contadorRespuestasCorrectas++;
+                }
+            }
+        }
+        return (contadorRespuestasCorrectas > 0);
+    }
 }
+
