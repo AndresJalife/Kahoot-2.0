5c3487d0fe5502de41d50573acf5d1491a4fb475
Zaphyk <zaphyk@live.com>
2020-08-05 20:01:12 -0300

    añadir text unitario base de ronda

---
+++ src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestUnitarioRonda.java
@@ -0,0 +1,31 @@
+package edu.fiuba.algo3.modelo.TestUnitarios;
+
+import org.junit.jupiter.api.Test;
+
+public class TestUnitarioRonda {
+
+    @Test
+    public void TestRondaDevuelvePreguntaActual() {
+
+    }
+
+    @Test
+    public void TestRondaDevuelveOpcionesCorrectas() {
+
+    }
+
+    @Test
+    public void TestRondaDevuelveOpcionesCorrectas() {
+
+    }
+
+    @Test
+    public void TestRondaNormalActualizaElPuntaje() {
+
+    }
+
+    @Test
+    public void TestRondaExclusividadActualizaElPuntaje() {
+
+    }
+}
