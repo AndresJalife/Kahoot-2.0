3187e2c8e3c8b7bdb5314212abaf3b66ec78ac85
Andres Jalife <ajalife@fi.uba.ar>
2020-08-10 22:54:02 -0300

    new interface and class for opening question files

--- src/main/java/edu/fiuba/algo3/modelo/general/LectorDeArchivos.java
+++ src/main/java/edu/fiuba/algo3/modelo/general/LectorDeArchivos.java
@@ -5,26 +5,10 @@ import edu.fiuba.algo3.modelo.preguntas.Opcion;
 import edu.fiuba.algo3.modelo.preguntas.Pregunta;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
 
 public interface LectorDeArchivos {
-    List<Pregunta> crearListaDePreguntas(String archivo);
-    File archivoPreguntas = new File(nombreArchivo);
-    Scanner sc = new Scanner(archivoPreguntas);
-
-        while (sc.hasNext()){
-        String textoPregunta = sc.nextLine();
-        String[] datos = textoPregunta.split("/");
-
-        FabricaDePreguntas fabricaDePreguntas = new FabricaDePreguntas();
-
-        String tipo = datos[1];
-        String modo = datos[0];
-        String pregunta = datos[2];
-        List<Opcion> opcionesPosibles = separarOpciones(datos[3], datos[4]);
-
-        Pregunta nuevaPregunta = fabricaDePreguntas.crearPregunta(pregunta, opcionesPosibles, tipo, modo);
-        preguntas.add(nuevaPregunta);
-    }
+    List<Pregunta> crearListaDePreguntas(String nombreArchivo) throws FileNotFoundException;
 }
--- src/main/java/edu/fiuba/algo3/modelo/general/LectorDeArchivosTxt.java
+++ src/main/java/edu/fiuba/algo3/modelo/general/LectorDeArchivosTxt.java
@@ -1,12 +1,55 @@
 package edu.fiuba.algo3.modelo.general;
 
+import edu.fiuba.algo3.modelo.preguntas.FabricaDePreguntas;
+import edu.fiuba.algo3.modelo.preguntas.Opcion;
 import edu.fiuba.algo3.modelo.preguntas.Pregunta;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class LectorDeArchivosTxt implements LectorDeArchivos{
     @Override
-    public List<Pregunta> crearListaDePreguntas(String archivo) {
-        return null;
+    public List<Pregunta> crearListaDePreguntas(String nombreArchivo) throws FileNotFoundException {
+        List<Pregunta> nuevasPreguntas = new ArrayList<Pregunta>();
+
+        File archivoPreguntas = new File(nombreArchivo);
+        Scanner sc = new Scanner(archivoPreguntas);
+
+        while (sc.hasNext()){
+            String textoPregunta = sc.nextLine();
+            String[] datos = textoPregunta.split("/");
+
+            FabricaDePreguntas fabricaDePreguntas = new FabricaDePreguntas();
+
+            String tipo = datos[1];
+            String modo = datos[0];
+            String pregunta = datos[2];
+            List<Opcion> opcionesPosibles = separarOpciones(datos[3], datos[4]);
+
+            Pregunta nuevaPregunta = fabricaDePreguntas.crearPregunta(pregunta, opcionesPosibles, tipo, modo);
+            nuevasPreguntas.add(nuevaPregunta);
+        }
+        return nuevasPreguntas;
+    }
+    private List<Opcion> separarOpciones(String posibles, String correcta){
+        String[] opcionesSeparadas = posibles.split(",");
+        List<String> listaPosibles = Arrays.asList(opcionesSeparadas);
+        List<Opcion> listaOpciones = new ArrayList<>();
+
+        for (String opcion:listaPosibles){
+            Opcion nuevaOpcion;
+            if (opcion.equals(correcta)){
+                nuevaOpcion = new Opcion(opcion, true);
+            }
+            else{
+                nuevaOpcion = new Opcion(opcion);
+            }
+            listaOpciones.add(nuevaOpcion);
+        }
+        return listaOpciones;
     }
 }
