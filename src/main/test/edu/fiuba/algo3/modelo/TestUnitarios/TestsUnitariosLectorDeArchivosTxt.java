b5acccc198e19cb40b1ab8b27f03f1cdfd21f17b
Andres Jalife <ajalife@fi.uba.ar>
2020-08-10 23:05:54 -0300

    Tests for lectorDeArchivos

--- src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestsUnitariosLectorDeArchivosTxt.java
+++ src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestsUnitariosLectorDeArchivosTxt.java
@@ -1,2 +1,22 @@
-package edu.fiuba.algo3.modelo.TestUnitarios;public class TestsUnitariosLectorDeArchivosTxt {
+package edu.fiuba.algo3.modelo.TestUnitarios;
+
+import edu.fiuba.algo3.modelo.general.LectorDeArchivosTxt;
+import edu.fiuba.algo3.modelo.preguntas.Pregunta;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestsUnitariosLectorDeArchivosTxt {
+    LectorDeArchivosTxt lectorDeArchivosTxt = new LectorDeArchivosTxt();
+
+    @Test
+    public void cargoUnaPreguntaYEsSeCreaCorrectamente() throws FileNotFoundException {
+        List<Pregunta> preguntas = lectorDeArchivosTxt.crearListaDePreguntas("./unaPregunta.txt");
+        Pregunta pregunta = preguntas.get(0);
+
+        assertEquals(pregunta.obtenerTexto(), "¿Como se llama tu mascota?");
+    }
 }
