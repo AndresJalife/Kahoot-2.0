d989bc80fef1e0ac782592337be675b34d960b2b
Mathias <mwelz@fi.uba.ar>
2020-08-13 00:53:49 -0300

    arreglo OrderedChoice

--- src/main/java/edu/fiuba/algo3/view/preguntas/VistaOrderedChoice.java
+++ src/main/java/edu/fiuba/algo3/view/preguntas/VistaOrderedChoice.java
@@ -5,10 +5,7 @@ import edu.fiuba.algo3.modelo.general.Kahoot;
 import edu.fiuba.algo3.modelo.general.RespuestasPorJugador;
 
 public class VistaOrderedChoice extends VistaTipoDePregunta {
-    private RespuestasPorJugador respuestasPorJugador;
     public VistaOrderedChoice(RespuestasPorJugador respuestasPorJugador){
-        this.respuestasPorJugador = respuestasPorJugador;
-
     }
 
     public VistaOrderedChoice(Kahoot modelo, Jugador jugador) {
