4a25fd242c5460da3237ae6fc5505f1a66e6b014
Andres Jalife <ajalife@fi.uba.ar>
2020-08-13 17:41:48 -0300

    trying css styling

--- src/main/java/edu/fiuba/algo3/view/VistaMenu.java
+++ src/main/java/edu/fiuba/algo3/view/VistaMenu.java
@@ -48,7 +48,7 @@ public class VistaMenu extends StackPane {
 
     private void obtenerIngresarArchivo(Kahoot modelo, Stage stage, GridPane grid) {
         try {
-            modelo.inicializarPreguntas(getClass().getResource("/semiPreguntas.json").toURI());
+            modelo.inicializarPreguntas(getClass().getResource("/semiPreguntas2.json").toURI());
 
         }
         catch (Exception e){
