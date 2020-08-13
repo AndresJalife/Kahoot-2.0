127a83b7ddbbe21b3fb47b9472cfa016ac58ad03
Andres Jalife <ajalife@fi.uba.ar>
2020-08-12 23:15:24 -0300

    add unit test for json reader

---
+++ src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestsUnitariosLectorDeArchivosJson.java
@@ -0,0 +1,23 @@
+package edu.fiuba.algo3.modelo.TestUnitarios;
+
+import edu.fiuba.algo3.modelo.general.LectorDeArchivosTxt;
+import edu.fiuba.algo3.modelo.preguntas.Pregunta;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestsUnitariosLectorDeArchivosTxt {
+    LectorDeArchivosTxt lectorDeArchivosTxt = new LectorDeArchivosTxt();
+
+    @Test
+    public void cargoUnaPreguntaYEsSeCreaCorrectamente() throws FileNotFoundException {
+//        List<Pregunta> preguntas = lectorDeArchivosTxt.crearListaDePreguntas("/home/andres/Desktop/Sistemas/Algoritmos y Programacion/Algoritmos y Programacion 3/TP2-Algo3/src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/unaPregunta.txt");
+//        Pregunta pregunta = preguntas.get(0);
+//        System.out.println(pregunta.getClass().getName());
+//        assertEquals(pregunta.obtenerTexto(), "¿Como se llama tu mascota?");
+////        assertEquals(pregunta.getClass().getName(),"Pregunta");
+    }
+}
