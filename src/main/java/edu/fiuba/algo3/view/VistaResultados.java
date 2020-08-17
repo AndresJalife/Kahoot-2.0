73a36b92c6fc18531840f017e33a45037c4c920a
DamianGanopolsky <damian98dgg@gmail.com>
2020-08-16 23:37:28 -0300

    Agrego Vista Final

---
+++ src/main/java/edu/fiuba/algo3/view/VistaResultados.java
@@ -0,0 +1,21 @@
+package edu.fiuba.algo3.view;
+
+import edu.fiuba.algo3.modelo.general.Jugador;
+import edu.fiuba.algo3.modelo.general.Kahoot;
+import edu.fiuba.algo3.view.preguntas.VistaVerdaderoYFalso;
+import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+import java.util.*;
+
+public class VistaResultados extends StackPane{
+
+
+}
