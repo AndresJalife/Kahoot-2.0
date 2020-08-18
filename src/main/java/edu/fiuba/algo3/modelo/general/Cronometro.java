e754eb8975423a664e56800a2400ac0a1871bc56
Zaphyk <zaphyk@live.com>
2020-08-17 23:10:36 -0300

    usar threads para el cronometro

--- src/main/java/edu/fiuba/algo3/modelo/general/Cronometro.java
+++ src/main/java/edu/fiuba/algo3/modelo/general/Cronometro.java
@@ -1,4 +1,35 @@
 package edu.fiuba.algo3.modelo.general;
 
 public class Cronometro {
+    private int segundos;
+    private CronometroThread thread;
+    private boolean estaCorriendo;
+    private boolean termino;
+
+    public Cronometro(int segundos) {
+        this.segundos = segundos;
+        this.reset();
+    }
+
+    public void reset() {
+        thread = new CronometroThread(segundos);
+        thread.setAlCambiar(this::alCambiar);
+    }
+
+    public void comenzar() {
+        estaCorriendo = true;
+        thread.start();
+    }
+
+    private void alCambiar(int segundos) {
+        if (this.segundos == 0) {
+            estaCorriendo = false;
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(segundos);
+    }
 }
--- src/main/java/edu/fiuba/algo3/modelo/general/CronometroThread.java
+++ src/main/java/edu/fiuba/algo3/modelo/general/CronometroThread.java
@@ -1,8 +1,33 @@
 package edu.fiuba.algo3.modelo.general;
 
-public class CronometroRunnable implements Runnable {
+import java.util.function.Consumer;
+
+public class CronometroThread extends Thread {
+
+    private int segundos;
+    private Consumer<Integer> alCambiar;
+
+    public CronometroThread(int segundos) {
+        this.segundos = segundos;
+    }
+
     @Override
     public void run() {
-        
+        segundos--;
+        if(this.alCambiar != null)
+            this.alCambiar.accept(this.segundos);
+        esperar1Segundo();
+    }
+
+    private void esperar1Segundo() {
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setAlCambiar(Consumer<Integer> handler) {
+        this.alCambiar = handler;
     }
 }
