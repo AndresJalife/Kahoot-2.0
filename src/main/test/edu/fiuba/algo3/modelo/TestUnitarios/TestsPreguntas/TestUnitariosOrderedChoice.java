e1a2141c356aed2b127612cb79de459ad122e0a1
DamianGanopolsky <damian98dgg@gmail.com>
2020-08-18 20:24:20 -0300

    Actualizo tests ordered choice

--- src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestsPreguntas/TestUnitariosOrderedChoice.java
+++ src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestsPreguntas/TestUnitariosOrderedChoice.java
@@ -14,13 +14,17 @@ public class TestUnitariosOrderedChoice {
     List<Opcion> opciones = new ArrayList<>();
     Pregunta pregunta ;
     Jugador jugador = new Jugador("Jorge");
-    Opcion Opcion1 = new Opcion("Primera", true);
-    Opcion Opcion2 = new Opcion("Segunda", true);
-    Opcion Opcion3 = new Opcion("Tercera", true);
+  //  Opcion Opcion1 = new Opcion("Primera", true);
+    //Opcion Opcion2 = new Opcion("Segunda", true);
+    //Opcion Opcion3 = new Opcion("Tercera", true);
+
 
 
     @Test
     public void ingresarRespuestasOrdenadasCorrectamente() {
+        Opcion Opcion1 = new Opcion("Primera",1,1);
+        Opcion Opcion2 = new Opcion("Segunda", 2,2);
+        Opcion Opcion3 = new Opcion("Tercera", 3,3);
         opciones.add(Opcion1);
         opciones.add(Opcion2);
         opciones.add(Opcion3);
@@ -30,14 +34,17 @@ public class TestUnitariosOrderedChoice {
     }
     @Test
     public void ingresarTodasLasOpcionesEnOrdenCorrectoDevuelve1PuntoYSeLoAsignaAJugador() {
+        Opcion Opcion1 = new Opcion("Primera",1,1);
+        Opcion Opcion2 = new Opcion("Segunda", 2,2);
+        Opcion Opcion3 = new Opcion("Tercera", 3,3);
         opciones.add(Opcion1);
         opciones.add(Opcion2);
         opciones.add(Opcion3);
         pregunta = new Pregunta("Ordenar de forma ascendente", opciones, new TipoOrderedChoice(), new ModoClasico());
 
-        respuestas.add(new RespuestaDeJugador(Opcion1));
-        respuestas.add(new RespuestaDeJugador(Opcion2));
-        respuestas.add(new RespuestaDeJugador(Opcion3));
+        respuestas.add(new RespuestaDeJugador(Opcion1,1));
+        respuestas.add(new RespuestaDeJugador(Opcion2,2));
+        respuestas.add(new RespuestaDeJugador(Opcion3,3));
 
         jugador.agregarPuntaje(pregunta.calcularPuntaje(respuestas));
 
@@ -45,6 +52,9 @@ public class TestUnitariosOrderedChoice {
     }
     @Test
     public void ingresar1OpcionEnOrdenCorrectoDevuelve0puntosYalAgregarleAJugadorNoCambiaSuPuntaje() {
+        Opcion Opcion1 = new Opcion("Primera",1,1);
+        Opcion Opcion2 = new Opcion("Segunda", 2,3);
+        Opcion Opcion3 = new Opcion("Tercera", 3,2);
         opciones.add(Opcion1);
         opciones.add(Opcion2);
         opciones.add(Opcion3);
