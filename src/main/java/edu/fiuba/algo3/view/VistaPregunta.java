bddf87b77a7499c061c442d2acb07b8ea23f955b
Zaphyk <zaphyk@live.com>
2020-08-18 00:18:33 -0300

    añadir counter

--- src/main/java/edu/fiuba/algo3/view/VistaPregunta.java
+++ src/main/java/edu/fiuba/algo3/view/VistaPregunta.java
@@ -6,39 +6,63 @@ import edu.fiuba.algo3.view.preguntas.FabricaDeVistasPregunta;
 import edu.fiuba.algo3.view.preguntas.VistaTipoDePregunta;
 import edu.fiuba.algo3.view.preguntas.VistaVerdaderoYFalso;
 import edu.fiuba.algo3.view.VistaRespuestas;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
 
 public class VistaPregunta extends StackPane {
 
-    FabricaDeVistasPregunta fabrica = new FabricaDeVistasPregunta();
-    public VistaPregunta(){}
+    private final FabricaDeVistasPregunta fabrica;
+    private Label contador;
+
+    public VistaPregunta() {
+        fabrica = new FabricaDeVistasPregunta();
+    }
 
     public VistaPregunta(Kahoot modelo , Jugador jugador, Stage stage) {
-        modelo.comenzarRonda();
-        VistaTipoDePregunta vistaTipoPregunta = fabrica.crearVistaTipoDePregunta(modelo,jugador,stage);
-        this.getChildren().addAll(vistaTipoPregunta);
+        this();
+
+        var vistaTipoPregunta = fabrica.crearVistaTipoDePregunta(modelo, jugador, stage);
+        var contador = new Label();
+        contador.setFont(Font.font("Arial", FontWeight.BOLD, 28));
+        //setMargin(contador, new Insets(-200, -100, 0, 0));
+        contador.setTranslateX(-400);
+        contador.setTranslateY(-350);
+
+        var alCambiar = (Consumer<Integer>) (segundos) -> {
+            Platform.runLater( () -> {
+                contador.setText(segundos.toString());
+            });
+        };
+        var alTerminar = (Runnable) () -> {
+            // Hacer que pase la pregunta
+        };
+        modelo.comenzarRonda(alCambiar, alTerminar);
+
+        this.getChildren().addAll(vistaTipoPregunta, contador);
     }
 
-    public void VistaPreguntaSiguiente(Kahoot modelo , Jugador jugador, Stage stage) {
-        VistaTipoDePregunta vistaTipoPregunta = fabrica.crearVistaTipoDePregunta(modelo,jugador,stage);
-        this.getChildren().addAll(vistaTipoPregunta);
-    }
-
-    public void CambiarPreguntaAOtroJugador(Kahoot modelo, Jugador jugador, Stage stage){
+    public void CambiarPreguntaAOtroJugador(Kahoot modelo, Jugador jugador, Stage stage) {
         this.getChildren().clear();
-        if(modelo.todosContestaron()){
-            if(modelo.esRondaFinal()){
+        if(modelo.todosContestaron()) {
+            if(modelo.esRondaFinal()) {
                 VistaResultados vistaResultados = new VistaResultados(modelo,stage);
-                this.getChildren().addAll(vistaResultados);}
+                this.getChildren().addAll(vistaResultados);
+            }
             else {
                 VistaRespuestas vistaRespuesta = new VistaRespuestas(modelo, stage);
-                this.getChildren().addAll(vistaRespuesta);}
+                this.getChildren().addAll(vistaRespuesta);
+            }
         }
         else{
             Jugador siguiente = jugador;
