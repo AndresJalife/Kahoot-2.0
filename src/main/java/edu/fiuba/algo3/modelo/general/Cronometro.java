5d7602e00baca43533d076e3f02d4ca4bfb83a05
Zaphyk <zaphyk@live.com>
2020-08-18 00:18:15 -0300

    permitir que el cronometro tome callbacks de afuera

--- src/main/java/edu/fiuba/algo3/modelo/general/Cronometro.java
+++ src/main/java/edu/fiuba/algo3/modelo/general/Cronometro.java
@@ -1,10 +1,12 @@
 package edu.fiuba.algo3.modelo.general;
 
+import java.util.function.Consumer;
+
 public class Cronometro {
     private int segundos;
     private CronometroThread thread;
-    private boolean estaCorriendo;
-    private boolean termino;
+    private Consumer<Integer> alCambiar;
+    private Runnable alTerminar;
 
     public Cronometro(int segundos) {
         this.segundos = segundos;
@@ -13,23 +15,19 @@ public class Cronometro {
 
     public void reset() {
         thread = new CronometroThread(segundos);
-        thread.setAlCambiar(this::alCambiar);
+        thread.setAlCambiar(this.alCambiar);
+        thread.setAlTerminar(this.alTerminar);
     }
 
     public void comenzar() {
-        estaCorriendo = true;
         thread.start();
     }
 
-    private void alCambiar(int segundos) {
-        if (this.segundos == 0) {
-            estaCorriendo = false;
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println(segundos);
+    public void setAlCambiar(Consumer<Integer> handler) {
+        this.alCambiar = handler;
+    }
+
+    public void setAlTerminar(Runnable handler) {
+        this.alTerminar = handler;
     }
 }
