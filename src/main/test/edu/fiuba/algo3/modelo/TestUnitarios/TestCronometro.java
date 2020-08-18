4faaf42e89ce86143121b0c8a3bfebce084d5080
Zaphyk <zaphyk@live.com>
2020-08-18 18:13:37 -0300

    añadir tests base para el cronometro

---
+++ src/main/test/edu/fiuba/algo3/modelo/TestUnitarios/TestCronometro.java
@@ -0,0 +1,31 @@
+package edu.fiuba.algo3.modelo.TestUnitarios;
+
+import org.junit.Test;
+
+public class TestCronometro {
+
+    @Test
+    public void TestCronometroTardaSegundos() {
+
+    }
+
+    @Test
+    public void TestCronometroLlamaCallbackAlFinalizar() {
+
+    }
+
+    @Test
+    public void TestCronometroLlamaCadaSegundo() {
+
+    }
+
+    @Test
+    public void TestCronometroSePuedeCortar() {
+
+    }
+
+    @Test
+    public void TestCronometroSePuedeResetear() {
+
+    }
+}
