e41688482058c996d5a3252ecc82533cf17b012d
Zaphyk <zaphyk@live.com>
2020-08-18 01:28:06 -0300

    reusar vista pregunta para q se muestre el cronometro

--- src/main/java/edu/fiuba/algo3/view/eventos/ReCrearLaPregunta.java
+++ src/main/java/edu/fiuba/algo3/view/eventos/ReCrearLaPregunta.java
@@ -3,6 +3,7 @@ package edu.fiuba.algo3.view.eventos;
 import edu.fiuba.algo3.modelo.general.Jugador;
 import edu.fiuba.algo3.modelo.general.Kahoot;
 import edu.fiuba.algo3.modelo.preguntas.FabricaDePreguntas;
+import edu.fiuba.algo3.view.VistaPregunta;
 import edu.fiuba.algo3.view.preguntas.FabricaDeVistasPregunta;
 import edu.fiuba.algo3.view.preguntas.VistaTipoDePregunta;
 import javafx.event.ActionEvent;
@@ -13,22 +14,20 @@ import javafx.stage.Stage;
 public class ReCrearLaPregunta implements EventHandler<ActionEvent> {
 
     Stage miStage;
-    FabricaDeVistasPregunta miFabrica;
     Kahoot miModelo;
     Jugador miJugador;
 
     public ReCrearLaPregunta(Stage stage , Kahoot modelo, Jugador jugador){
         miStage = stage;
-        miFabrica = new FabricaDeVistasPregunta();
         miModelo = modelo;
         miJugador = jugador;
     }
 
     @Override
     public void handle(ActionEvent actionEvent) {
-        VistaTipoDePregunta vistaTipoPregunta = miFabrica.crearVistaTipoDePregunta(miModelo,miJugador,miStage);
-        Scene escena = new Scene(vistaTipoPregunta);
-        escena.getStylesheets().add(getClass().getResource("/css/escenaInicial.css").toExternalForm());
+        var vistaPregunta = new VistaPregunta(miModelo, miJugador, miStage);
+        var escena = new Scene(vistaPregunta);
+        escena.getStylesheets().add(getClass().getResource("/css/escenaPregunta.css").toExternalForm());
         miStage.setScene(escena);
     }
 }
