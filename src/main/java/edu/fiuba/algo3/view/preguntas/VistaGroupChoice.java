0995e6bc2dd00ed863944873b72a23cc55f988ac
Mathias <mwelz@fi.uba.ar>
2020-08-18 00:39:35 -0300

    vista GroupChoice compila y funciona

--- src/main/java/edu/fiuba/algo3/view/preguntas/VistaGroupChoice.java
+++ src/main/java/edu/fiuba/algo3/view/preguntas/VistaGroupChoice.java
@@ -7,6 +7,7 @@ import edu.fiuba.algo3.modelo.preguntas.Opcion;
 import edu.fiuba.algo3.modelo.preguntas.Pregunta;
 import edu.fiuba.algo3.modelo.preguntas.RespuestaDeJugador;
 import edu.fiuba.algo3.view.VistaPregunta;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -33,9 +34,10 @@ public class VistaGroupChoice extends VistaTipoDePregunta {
         super();
         this.getChildren().clear();
         inicializarTextos(modelo,jugador);
+
         setStackPane();
         this.setColorFondo();
-            botonAceptar.setOnAction(actionEvent -> {
+        botonAceptar.setOnAction(actionEvent -> {
                 modelo.jugadorResponder(jugador, respuestas);
                 VistaPregunta vistaAux = new VistaPregunta();
                 vistaAux.CambiarPreguntaAOtroJugador(modelo, jugador, stage);
@@ -43,9 +45,7 @@ public class VistaGroupChoice extends VistaTipoDePregunta {
                 this.getChildren().addAll(vistaAux);
             });
             this.getStylesheets().add(getClass().getResource("/css/escenaPregunta.css").toExternalForm());
-
-        }
-
+    }
     private void setStackPane() {
         this.setAlignment(Pos.CENTER);
 
@@ -56,7 +56,7 @@ public class VistaGroupChoice extends VistaTipoDePregunta {
         this.setMargin(preguntaTexto, new Insets(-500, 0, 0, 0));
 
         this.getChildren().add(vBoxOpciones);
-
+        this.setMargin(vBoxOpciones, new Insets(-400, 0, 0, 0));
         this.getChildren().add(botonAceptar);
         this.setMargin(botonAceptar, new Insets(0, 0, 0, 0));
 
@@ -64,14 +64,15 @@ public class VistaGroupChoice extends VistaTipoDePregunta {
 
     private void inicializarTextos(Kahoot modelo,Jugador jugador) {
         Pregunta pregunta = modelo.obtenerPreguntaActual();
-        nombreJugador.setText(jugador.obtenerNombre());
+        nombreJugador = new Label(jugador.obtenerNombre());
         nombreJugador.setFont(Font.font("Arial", FontWeight.BOLD, 35));
-        preguntaTexto.setText(pregunta.obtenerTexto());
+        preguntaTexto = new Label(pregunta.obtenerTexto());
         preguntaTexto.setFont(new Font(20));
 
         botonAceptar = new Button("Aceptar");
         botonAceptar.setAlignment(Pos.CENTER);
 
+        listaGrupos = FXCollections.observableArrayList();
 
         listaGrupos.add(1);
         listaGrupos.add(2);
@@ -80,7 +81,7 @@ public class VistaGroupChoice extends VistaTipoDePregunta {
         vBoxOpciones = new VBox();
         for (Opcion opcion : pregunta.obtenerOpcionesMezcladas()) {
             RespuestaDeJugador nuevaRespuestaDeJugador = new RespuestaDeJugador(opcion);
-            respuestas.add(new RespuestaDeJugador(opcion));
+
             ChoiceBox<Integer> nuevaChoiceBox = new ChoiceBox<>(listaGrupos);
             Label nuevaOpcionTexto = new Label(opcion.obtenerTexto());
 
@@ -89,18 +90,20 @@ public class VistaGroupChoice extends VistaTipoDePregunta {
 
             HBox nuevaCajaOpcion = new HBox(nuevaOpcionTexto, nuevaChoiceBox);
             nuevaCajaOpcion.setAlignment(Pos.CENTER);
-            nuevaChoiceBox.setValue(1);
 
-            nuevaChoiceBox.setOnAction(new ControladorSeleccionGrupos(nuevaRespuestaDeJugador, nuevaChoiceBox));
+            nuevaChoiceBox.setOnAction(new ControladorSeleccionGrupos(nuevaRespuestaDeJugador,nuevaChoiceBox));
 
             HBox cajaOpcionYChoiceBox = new HBox(nuevaOpcionTexto,nuevaChoiceBox);
+            cajaOpcionYChoiceBox.setAlignment(Pos.CENTER);
             vBoxOpciones.getChildren().add(cajaOpcionYChoiceBox);
-            vBoxOpciones.setFillWidth(true);
+            vBoxOpciones.setAlignment(Pos.CENTER);
+
+            respuestas.add(nuevaRespuestaDeJugador);
         }
     }
 
     private void setColorFondo() {
-        Color color = Color.web("#eecd86",1.0);
+        Color color = Color.rgb(122,62,72);
         this.setBackground(new Background((new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY))));
     }
 }
